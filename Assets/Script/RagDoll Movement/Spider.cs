9709ea0c91593c86a53784d88f550b84c5bb9158
floris224 <floris_2@hotmail.nl>
2024-05-23 02:43:59 +0200

    Spider Rigged / Now being able to stand
    Rigged AN put an ik Together on spidern ow just changing some values and it could be used as an enemy

---
+++ Assets/Script/RagDoll Movement/Spider.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : MonoBehaviour
+{
+   
+    public Rigidbody body;
+    public Transform desiredPosition;
+    public float porportionalGain;
+    public float intergralGain,intergralClamp;
+    public float devirativeGain;
+    private Vector3 previousError;
+    private Vector3 intergral;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+        Vector3 currentError = desiredPosition.position - body.position;
+        intergral += currentError * Time.fixedDeltaTime;
+        intergral = Vector3.ClampMagnitude(intergral,intergralClamp);
+        Vector3 derivative = (currentError - previousError) / Time.fixedDeltaTime;
+        previousError = currentError;
+        Vector3 force = (porportionalGain * currentError) + (intergralGain * intergral) + (devirativeGain * derivative);
+        body.AddForce(force);
+    }
+}
