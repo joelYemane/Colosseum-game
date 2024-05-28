bf325784171a2c7b6493b885b07d98f547984f08
floris224 <floris_2@hotmail.nl>
2024-05-28 22:10:22 +0200

    ragdoll walks alot better
    attack is still problem ik does not like to work with ragdoll. late update is it not

--- Assets/Script/RagDoll Movement/FeetPlacement.cs
+++ Assets/Script/RagDoll Movement/FeetPlacement.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class FeetPlacement : MonoBehaviour
 {
+    // attack
+    public float force;
+    public GameObject sword;
+    public Rigidbody arm;
+    public float attackCooldown;
+    public GameObject IKTargetLeft;
+    public GameObject IKTargetRight;
+    public Transform[] attackPoints;
+    public float attackSpeed;
     //Ragdoll Walk Force
-   
+
     public Transform player;
     public float stoppingDistance;
     public float multiplier;
@@ -20,9 +31,8 @@ public class FeetPlacement : MonoBehaviour
     public LayerMask ground;
     public Transform raycastL;
     public Transform raycastR;
-
+    public float distance;
     
-
     //LegSwitch
     private enum Foot { Left, Right }
     private Foot movingFoot = Foot.Left;
@@ -39,58 +49,71 @@ public class FeetPlacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        attackCooldown -= Time.deltaTime;
+        if (attackCooldown <= 0)
+        {
+            Debug.Log("Slam");
+           
+            attackCooldown = 5;
+        }
+
         transform.LookAt(player);
-        MoveTowardsPlayer();
+        Debug.Log(movingFoot);
+        
+            //Vector3 centerOfMass = CenterOfMass(ragdollRigidbodies);
+            if (stepTimer <= 0)
+            {
+                movingFoot = movingFoot == Foot.Left ? Foot.Right : Foot.Left;
+                stepTimer = stepDuration;
+
+            }
+            if (movingFoot == Foot.Left)
+            {
+                Vector3 leftFootGroundPos = GroundPos(raycastL.position);
+                Vector3 leftFootTarget = new Vector3(leftFootGroundPos.x + 0.4f, 0, leftFootGroundPos.z);
+                leftFootTargetIK.localPosition = Vector3.Lerp(leftFootTargetIK.position, leftFootTarget, Time.deltaTime * lerpSpeed);
+            }
+            else
+            {
+                Vector3 rightFoorGroundPos = GroundPos(raycastR.position);
+                Vector3 rightFootTarget = new Vector3(rightFoorGroundPos.x - 0.4f, 0, rightFoorGroundPos.z);
+                rightFootTargetIK.localPosition = Vector3.Lerp(rightFootTargetIK.position, rightFootTarget, Time.deltaTime * lerpSpeed);
+            }
+
+       
+        stepTimer -= Time.deltaTime;
+
 
     }
     public void FixedUpdate()
     {
+        MoveTowardsPlayer();
         
+
+
     }
 
     private void LateUpdate()
     {
         
-        stepTimer -= Time.deltaTime;
-        //Vector3 centerOfMass = CenterOfMass(ragdollRigidbodies);
-        if (stepTimer <= 0)
-        {
-            movingFoot = movingFoot == Foot.Left ? Foot.Right : Foot.Left;
-            stepTimer = stepDuration;
-
-        }
-        if (movingFoot == Foot.Left)
-        {
-            Vector3 leftFootGroundPos = GroundPos(raycastL.position);
-            Vector3 leftFootTarget = new Vector3(leftFootGroundPos.x + 0.4f, leftFootGroundPos.y, leftFootGroundPos.z);
-            leftFootTargetIK.localPosition = Vector3.Lerp(leftFootTargetIK.position, leftFootTarget, Time.deltaTime * lerpSpeed);
-        }
-        else
-        {
-            Vector3 rightFoorGroundPos = GroundPos(raycastR.position);
-            Vector3 rightFootTarget = new Vector3(rightFoorGroundPos.x - 0.4f, rightFoorGroundPos.y, rightFoorGroundPos.z);
-            rightFootTargetIK.localPosition = Vector3.Lerp(rightFootTargetIK.position, rightFootTarget, Time.deltaTime * lerpSpeed);
-        }
-
-
     }
     
     public void MoveTowardsPlayer()
     {
         Vector3 direction = (player.position - transform.position);
-        float distance = Vector3.Distance(transform.position, player.position);
-        if(distance > stoppingDistance)
+        distance = Vector3.Distance(transform.position, player.position);
+        if(distance >= stoppingDistance)
         {
             foreach(Rigidbody rb in ragdollRigidbodies)
             {
                 rb.AddForce(direction* multiplier);
             }
         }
-        
-        if(distance<=stoppingDistance)
+        else
         {
             foreach (Rigidbody rb in ragdollRigidbodies)
             {
+                Debug.Log("Hit");
                 rb.velocity = Vector3.zero;
             }
         }
